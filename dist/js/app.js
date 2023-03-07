00683813fdf71661d81cda94ed6c62970c5f4705
oheneadj <oheneadjei.dev@gmail.com>
2023-03-07 11:50:24 +0000

    added nasal vowels

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,122 +1,123 @@
 console.log("Working");
 const digraphs = ["ts", "tsy", "dz", "kp", "gb", "ny"];
-const nasalVowels = ["aa", "ee", "ɛɛ", "ii", "ɔɔ", "oo", "ũ"];
+const nasalVowels = ["aa", "ee", "ɛɛ", "ii", "ɔɔ", "oo", "uu"];
 const vowels = ["Aa", "Ee", "Ɛɛ", "Ii", "Oo", "Ɔɔ", "Uu"];
 const consonants = [
-    "a",
-    "b",
-    "d",
-    "ɖ",
-    "e",
-    "ɛ",
-    "f",
-    "ɸ",
-    "g",
-    "ɣ",
-    "h",
-    "i",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "ɔ",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "β",
-    "w",
-    "y",
-    "z",
+  "a",
+  "b",
+  "d",
+  "ɖ",
+  "e",
+  "ɛ",
+  "f",
+  "ɸ",
+  "g",
+  "ɣ",
+  "h",
+  "i",
+  "k",
+  "l",
+  "m",
+  "n",
+  "ŋ",
+  "o",
+  "ɔ",
+  "p",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "β",
+  "w",
+  "y",
+  "z",
 ];
 
 //functions for displaying various sounds
 
 //digraphs
 digraphs.forEach((digraph) => {
-    const btn = document.querySelector(`.${digraph}`);
-    btn.addEventListener("click", () => {
-        stopSongs();
-        document.getElementById(digraph).play();
-    });
+  const btn = document.querySelector(`.${digraph}`);
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(digraph).play();
+  });
 });
 
 //Vowels
 vowels.forEach((vowel) => {
-    const btn = document.querySelector(`.${vowel}`);
-    btn.addEventListener("click", () => {
-        stopSongs();
-        document.getElementById(vowel).play();
-    });
+  const btn = document.querySelector(`.${vowel}`);
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(vowel).play();
+  });
 });
 
 //nasalVowels
 nasalVowels.forEach((nasalVowel) => {
-    const btn = document.querySelector(`.${nasalVowel}`);
-    btn.addEventListener("click", () => {
-        // stopSongs();
-        // document.getElementById(nasalVowel).play();
-    });
+  const btn = document.querySelector(`.${nasalVowel}`);
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(nasalVowel).play();
+  });
 });
 
 // Consonants
 consonants.forEach((consonant) => {
-    const btn = document.querySelector(`.${consonant}`);
+  const btn = document.querySelector(`.${consonant}`);
 
-    btn.addEventListener("click", () => {
-        stopSongs();
-        document.getElementById(consonant).play();
-    });
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(consonant).play();
+  });
 });
 
 // === Functions to stop sounds ===
 
 //Stop all sounds
 const stopSongs = () => {
-    stopDigraphSongs();
-    //    stopNasalVowelSongs();
-    stopVowelSongs();
-    stopConsonantSongs();
+  stopDigraphSongs();
+  //    stopNasalVowelSongs();
+  stopVowelSongs();
+  stopConsonantSongs();
 };
 
 //stop playing current digraphs sound
 const stopDigraphSongs = () => {
-    digraphs.forEach((digraph) => {
-        const song = document.getElementById(digraph);
+  digraphs.forEach((digraph) => {
+    const song = document.getElementById(digraph);
 
-        song.pause();
-        song.currentTime = 0;
-    });
+    song.pause();
+    song.currentTime = 0;
+  });
 };
 
 //stop playing current vowel sound
 const stopVowelSongs = () => {
-    vowels.forEach((vowel) => {
-        const song = document.getElementById(vowel);
+  vowels.forEach((vowel) => {
+    const song = document.getElementById(vowel);
 
-        song.pause();
-        song.currentTime = 0;
-    });
+    song.pause();
+    song.currentTime = 0;
+  });
 };
 
 //stop playing current nasalVowel sound
-//const stopNasalVowelSongs = () => {
-//    nasalVowels.forEach((nasalVowel) => {
-//        const song = document.getElementById(nasalVowel);
-//
-//        song.pause();
-//        song.currentTime = 0;
-//    });
-//};
+const stopNasalVowelSongs = () => {
+  nasalVowels.forEach((nasalVowel) => {
+    const song = document.getElementById(nasalVowel);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+};
 
 //stop playing current consonant sound
 const stopConsonantSongs = () => {
-    consonants.forEach((consonant) => {
-        const song = document.getElementById(consonant);
-        song.pause();
-        song.currentTime = 0;
-    });
+  consonants.forEach((consonant) => {
+    const song = document.getElementById(consonant);
+    song.pause();
+    song.currentTime = 0;
+  });
 };
